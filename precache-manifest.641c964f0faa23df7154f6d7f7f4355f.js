a8a3b2db8ee330f386356534d886169479b726bc
Loremar Marabillas <loremar5858@gmail.com>
2020-04-30 08:48:34 +0800

    Updates

---
+++ precache-manifest.641c964f0faa23df7154f6d7f7f4355f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c84fbbe69ccc9bfa1251fdcbb77caf6f",
+    "url": "/FCC-Pomodoro-Clock/index.html"
+  },
+  {
+    "revision": "d215d849c7459dd59655",
+    "url": "/FCC-Pomodoro-Clock/static/css/2.6389eabf.chunk.css"
+  },
+  {
+    "revision": "2ceed44c9ca621011d35",
+    "url": "/FCC-Pomodoro-Clock/static/css/main.eea55b19.chunk.css"
+  },
+  {
+    "revision": "d215d849c7459dd59655",
+    "url": "/FCC-Pomodoro-Clock/static/js/2.8e4d6d72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FCC-Pomodoro-Clock/static/js/2.8e4d6d72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ceed44c9ca621011d35",
+    "url": "/FCC-Pomodoro-Clock/static/js/main.cd809fed.chunk.js"
+  },
+  {
+    "revision": "bde674b64380041b89b8",
+    "url": "/FCC-Pomodoro-Clock/static/js/runtime-main.4050c4e4.js"
+  },
+  {
+    "revision": "ba3c420345125bb2171512587de9b979",
+    "url": "/FCC-Pomodoro-Clock/static/media/beep.ba3c4203.wav"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/FCC-Pomodoro-Clock/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/FCC-Pomodoro-Clock/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/FCC-Pomodoro-Clock/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/FCC-Pomodoro-Clock/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/FCC-Pomodoro-Clock/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
